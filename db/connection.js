949f6adfdc44f134f047f3e21cdcc066675bbfb9
Abram1r <103975640+Abram1r@users.noreply.github.com>
2023-06-12 16:42:42 -0500

    created votes table

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
 
-
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your Mysql username,
-    user: 'root',
-    //Your Mysql password
-    password: 'Reserve160!',
-    database: 'election'
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: '',
+  database: 'election'
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-
-// Not Found response for unmatched routes
-app.use((req, res) => {
+// Default response for any other request (Not Found)
+app.use((req, res) => { 
   res.status(404).end();
 });
 
